62fa8f92a8a9606a1844c8b6275abd833106db76
ItsPax <alexsieu14@gmail.com>
2017-09-21 08:16:25 -0600

    finished p37

--- problem37.py
+++ problem37.py
@@ -1,74 +1,71 @@
 import math
-def breakNum(n):
-    n = list(n)
-    numList = []
-    while len(n) >= 1:
-        numList.append(str(n))
-        n.remove(n[0])
-    return numList
-
-def mergeSort(alist):
-    if len(alist) > 1:
-        mid = len(alist)//2
-        left = alist[:mid]
-        right = alist[mid:]
-        mergeSort(left)
-        mergeSort(right)
-        [i,j,k] = [0,0,0]
-        while i < len(left) and j < len(right):
-            if left[i] > right[j]:
-                alist[k] = right[j]
-                j += 1
-            else:
-                alist[k] = left[i]
-                i += 1
-            k += 1
-        while i < len(left):
-            alist[k] = left[i]
-            i += 1
-            k += 1
-        while j < len(right):
-            alist[k] = right[j]
-            j += 1
-            k += 1
-
 # build a list of right truncatable primes
 # build a list of left truncatable primes
 # find the union of the list
 # what's my upper bound? or can I create a function that recursively keeps increasing the bound, saving data?
 
+def isPrime(n):
+# try greedy solution as opposed to flex SoA
+    for num in range(2,int(math.sqrt(n)+1)):
+        if n%num == 0:
+            return False
+    return True
+
+
 def leftTruncPrimes(numList):
-# gives left truncatable primes using whatever you've determined to be LTP
+# gives left truncatable primes using whatever you've determined to be LTP, until impossible
 # numList is list of strings
 # use strs instead of ints + radix 10 + list implementation
     newNums = []
     for num in numList:
-        numList.remove(num)
-        for str(odd) in range(1,10,2):
+        strList = [str(x) for x in [1,2,3,5,7,9]]
+        for odd in strList:
             if isPrime(int(odd+num)):
                 newNums.append(odd+num)
     return newNums
 
 
 def rightTruncPrimes(numList):
-# gives left truncatable primes using whatever you've determined to be LTP
+# gives right truncatable primes using whatever you've determined to be RTP, until impossible
 # numList is list of strings
 # use strs instead of ints + radix 10 + list implementation
     newNums = []
     for num in numList:
-        numList.remove(num)
-        for str(odd) in range(1,10,2): # have to str(rng)
+        strList = [str(x) for x in range(1,10,2)]
+        for odd in strList:
             if isPrime(int(num+odd)):
-                newNums.append(odd+num)
+                newNums.append(num+odd)
     return newNums
 
-def isPrime(n):
-    # try greedy solution as opposed to flex SoA
-    for num in range(2,int(math.sqrt(n)+1)):
-        if n%num == 0:
-            return True
-    return False
+def genRTP():
+    RTP = []
+    newNums = ['2','3','5','7']
+    while len(newNums) != 0:
+        newNums = rightTruncPrimes(newNums)
+        for num in newNums:
+            RTP.append(num)
+            print(num)
+    return RTP
 
-print(isPrime(17))
+def genLTP():
+    LTP = []
+    newNums = ['2','3','5','7']
+    while len(newNums) != 0:
+        newNums = leftTruncPrimes(newNums)
+        for num in newNums:
+            LTP.append(num)
+            print(num)
+    return LTP
 
+ltp = genLTP()
+rtp = genRTP()
+mySum = 0
+truncDict = {}
+for num in ltp:
+    truncDict[num] = True
+for num in rtp:
+    if num in truncDict:
+        mySum += int(num)
+
+print(mySum)
 
